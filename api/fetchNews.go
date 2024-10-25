7a1085c2af01f9142fd48f94878acf06123d26ab
J. Grimberg <github@joelgrimberg.nl>
2024-10-25 16:33:38 +0200

    bump

---
+++ api/fetchNews.go
@@ -0,0 +1,67 @@
+package api
+
+import (
+	"encoding/json"
+	"fmt"
+	"io"
+	"net/http"
+	"os"
+)
+
+type Article struct {
+	Title string `json:"title"`
+}
+
+type Response struct {
+	Articles []Article `json:"articles"`
+}
+
+type Configuration struct {
+	NewsAPIURL string `json:"newsAPIURL"` // Change to a single string
+}
+
+func FetchNews() ([]Article, error) {
+	file, err := os.Open("conf.json")
+	if err != nil {
+		return nil, fmt.Errorf("failed to open configuration file: %w", err)
+	}
+	defer file.Close()
+
+	decoder := json.NewDecoder(file)
+	configuration := Configuration{}
+	err = decoder.Decode(&configuration)
+	if err != nil {
+		return nil, fmt.Errorf("failed to decode configuration: %w", err)
+	}
+
+	if len(configuration.NewsAPIURL) == 0 {
+		return nil, fmt.Errorf("no URLs found in configuration")
+	}
+
+	url := configuration.NewsAPIURL
+
+	req, err := http.NewRequest("GET", url, nil)
+	if err != nil {
+		return nil, fmt.Errorf("error creating request: %w", err)
+	}
+
+	res, err := http.DefaultClient.Do(req)
+	if err != nil {
+		return nil, fmt.Errorf("error executing request: %w", err)
+	}
+	defer res.Body.Close()
+
+	body, err := io.ReadAll(res.Body)
+	if err != nil {
+		return nil, fmt.Errorf("error reading response body: %w", err)
+	}
+
+	// Unmarshal the JSON response
+	var data Response
+	err = json.Unmarshal(body, &data)
+	if err != nil {
+		return nil, fmt.Errorf("error unmarshalling response: %w", err)
+	}
+
+	return data.Articles, nil
+}
